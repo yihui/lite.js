d20198bb5cfd95b0c8bd47d57324aeb314b31a7f
Yihui Xie <xie@yihui.name>
2025-01-20 21:38:34 -0600

    use querySelectorAll() to simply the query

--- js/math-code.js
+++ js/math-code.js
@@ -1,8 +1,8 @@
 // <code>$math$</code> to \(math\), and <code>$$math$$</code> to $$math$$:
 // https://yihui.org/en/2018/07/latex-math-markdown/
-[...document.getElementsByTagName('code')].forEach(code => {
+document.querySelectorAll(':not(pre) > code:not(.nolatex)').forEach(code => {
   // skip <pre> tags and <code> that has children or the nolatex class
-  if (code.parentNode.tagName === 'PRE' || code.childElementCount > 0 || code.classList.contains('nolatex')) return;
+  if (code.childElementCount > 0) return;
   let text = code.textContent;
   if (/^\$[^$]/.test(text) && /[^$]\$$/.test(text)) {
     text = text.replace(/^\$/, '\\(').replace(/\$$/, '\\)');
