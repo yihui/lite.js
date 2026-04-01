c40ba571aa78bbbc4e32cc8adc94df38c0a4ea1c
Yihui Xie <xie@yihui.name>
2026-03-31 23:19:38 -0500

    increase legend marker size too

--- js/g2-patches.js
+++ js/g2-patches.js
@@ -36,6 +36,11 @@ G2.register("data.column", (options) => {
 
   const patchTheme = (theme) => {
     scaleFontSizes(theme);
+    if (
+      isObj(theme.legendCategory) &&
+      typeof theme.legendCategory.itemMarkerSize === "number"
+    )
+      theme.legendCategory.itemMarkerSize *= FONT_SCALE;
     if (isObj(theme.point))
       for (const style of Object.values(theme.point))
         if (isObj(style) && typeof style.r === "number")
