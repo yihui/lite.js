bbda49f56e898b7ad6baf3852eda2b289620ad39
Yihui Xie <xie@yihui.name>
2026-06-15 14:34:38 -0500

    Refactor dl-fieldset.js for cleaner syntax

--- js/dl-fieldset.js
+++ js/dl-fieldset.js
@@ -3,10 +3,10 @@
 // see documentation at: https://yihui.org/en/2023/11/dl-fieldset/
 document.querySelectorAll('dl').forEach(dl => {
   if (dl.childElementCount !== 2) return;
-  const dt = dl.children[0], dd = dl.children[1];
+  const [dt, dd] = dl.children;
   if (dt.tagName !== 'DT' || dd.tagName !== 'DD') return;
-  const fieldset = document.createElement('fieldset');
-  const legend = document.createElement('legend');
+  const fieldset = document.createElement('fieldset'),
+    legend = document.createElement('legend');
   legend.append(...dt.childNodes);
   fieldset.append(legend, ...dd.childNodes);
   dl.after(fieldset); dl.remove();
